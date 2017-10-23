e6a35cfb8922f441b71d2441a0c34dc6b1e27fa1
ikepon <tatsuyanoheya@gmail.com>
2017-10-23 22:25:43 +0900

    Add author name

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171023115049) do
+ActiveRecord::Schema.define(version: 20171023131240) do
 
   create_table "blorgh_articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "author_id"
   end
 
   create_table "blorgh_comments", force: :cascade do |t|
